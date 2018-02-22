a115e9393505fb7e68d8406e56855ecc0c93bf6b
sergio <scrummish@gmail.com>
2018-02-22 15:09:00 -0600

    Created initial mongoose models

---
+++ models/playerModel.js
@@ -0,0 +1,18 @@
+const 	mongoose = require('mongoose'),
+		Schema = mongoose.schema,
+		Win = require('./winsModel.js'),
+		Loss = require('./lossesModel.js')
+
+
+const playerSchema = new mongoose.Schema({
+	name: String, // ex. Limbo
+	alias: [String], // ex. EMPR TWISTED, Limbodawg, FishAndChips, EMPEROR_TWISTED
+	strengths: [String], // ['Defense', 'Zoning', 'Patience', 'Clutch Factor' ] determined by algorithm that will weigh all this players percieved strengths vs hers/his weaknesses
+	weaknesses: [String], // ['Offense','Set Play'] determined by algorithm that will weigh all this players percieved weaknesses vs his/hers percieved strengths 
+	notes: String, // ['Does not punish move x', 'Not up to date on DLC meta']
+	games: [String], // ['Mortal Kombat 9', 'Injustice: Gods Among Us', 'Injustice 2', 'Mortal Kombat X', 'Dragonball FighterZ']
+	losses: [Loss.schema], 
+	wins: [Win.schema]
+})
+
+module.exports = mongoose.model('Player', playerSchema);
---
+++ models/winsModel.js
@@ -0,0 +1,17 @@
+const 	mongoose = require('mongoose'),
+		Player = require('./playerModel.js'),
+		Schema = mongoose.schema;
+
+const winsSchema = new mongoose.Schema({
+	video: String, // link to youtube video of match
+	player: {type: [Player.schema], required: true}, // Player who owns this win
+	characterUsed: {type: String, required: true}, // Character Used to win
+	playerBeat: {type: [Player.schema], required: true}, // Player Beat
+	characterBeat: {type: String, required: true}, // Opponents character defeated
+	game: {type: String, required: true}, // What game was it
+	strengths: [String], // Predefined list of strengths shown in the match up
+	weaknesses: [String], // Predefined list of weaknesses shown in the match up
+	tournament: String // Predefined list of tournaments
+})
+
+module.exports = mongoose.model('Wins', winsSchema);
